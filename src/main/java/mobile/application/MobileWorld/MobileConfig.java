371364e027cfc91fe7726d47fd0b22bd5523a142
prabakarankanagaraj <prabakaran91577@gmail.com>
2022-09-02 15:55:24 +0530

    Database Postgresql has Changed

--- src/main/java/mobile/application/MobileWorld/MobileConfig.java
+++ src/main/java/mobile/application/MobileWorld/MobileConfig.java
@@ -1,6 +1,7 @@
 package mobile.application.MobileWorld;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,7 +38,7 @@ public class MobileConfig {
 					password("manager").roles("USER").build();
 			UserDetails myUser2=User.withDefaultPasswordEncoder().username("praba").
 					password("primeminister").roles("USER").build();
-			Collection<UserDetails> myusers=Stream.of(myUser1,myUser2).toList();
+			Collection<UserDetails> myusers=Stream.of(myUser1,myUser2).collect(Collectors.toList());
 				
 			return new InMemoryUserDetailsManager(myusers);
 			
